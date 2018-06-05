eb78ebd5f02ef437f7698247b011255304d61a79
Sarkar <sarkara1@ohio.edu>
2018-06-04 23:37:31 -0400

    initial commit

---
+++ src/test/java/OPL/oplrun/AppTest.java
@@ -0,0 +1,31 @@
+package OPL.oplrun;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import ilog.concert.IloException;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    @Test
+    public void testBouma1(){
+    	
+    	OPL o = new OPL("C:/Users/sarkara1/git/opl/minTT1pmtn/Bouma_BLP1.mod", "C:/Users/sarkara1/git/opl/minTT1pmtn/Bouma_BLP1.dat");
+    	
+    	try {
+			o.startEngine();
+	    	o.solve();
+	    	o.printResult();
+	    	//double[][][] x = o.getMatrix3DParam("x");
+	    	o.closeOPL();
+	    	//Assert.assertEquals(x.toString(), "[[[0.5, 0.0, 0.0, 0.0, 0.0, 0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0], [0.0, 0.5, 0.0, 0.0, 0.0, 0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0], [0.0, 0.0, 0.5, 0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]], [[0.5, 0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0], [0.0, 0.0, 0.5, 0.5, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0], [0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0]], [[0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0], [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0], [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0]], [[0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0], [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0], [0.0, 0.0, 0.0, 0.0, 0.0, 0.0, 1.0, 0.0, 0.0, 0.0, 0.0, 0.0]]]");
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			o.handleException(e);
+		}
+    	
+    }
+}
